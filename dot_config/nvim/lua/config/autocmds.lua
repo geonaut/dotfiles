b627c364b4bf3e31e03bfeba76a1ae797f26300c
Oliver Bazely <geonaut@geonaut.co.uk>
2026-03-25 10:26:28 +0000

    getting neovim setup as an editor

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,19 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Detect filetype for chezmoi .tmpl files based on the underlying extension
+vim.filetype.add({
+  pattern = {
+    [".*%.tmpl"] = function(path)
+      local name = vim.fn.fnamemodify(path:gsub("%.tmpl$", ""), ":t")
+      -- Convert chezmoi naming conventions to real filenames
+      name = name:gsub("^dot_", ".")
+      name = name:gsub("^private_", "")
+      name = name:gsub("^executable_", "")
+      name = name:gsub("^readonly_", "")
+      return vim.filetype.match({ filename = name })
+    end,
+  },
+})
+
